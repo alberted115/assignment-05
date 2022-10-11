a3a5c74a74b6f7b252efedc9585f228eeadca5b5
Jklo <jklo@itu.dk>
2022-10-11 12:38:42 +0200

    Deleted outcommented stuff

--- GildedRose/Program.cs
+++ GildedRose/Program.cs
@@ -9,7 +9,7 @@ namespace GildedRose
 
         static void Main(string[] args)
         {
-            /*System.Console.WriteLine("OMGHAI!");
+            System.Console.WriteLine("OMGHAI!");
 
             var app = new Program()
                           {
@@ -54,7 +54,7 @@ namespace GildedRose
                 }
                 Console.WriteLine("");
                 app.UpdateQuality();
-            }*/
+            }
 
         }
 
@@ -64,87 +64,10 @@ namespace GildedRose
             {
                 Items[i] = ItemFactory.CreateItem(Items[i]);
             }
-
             foreach (IItem item in Items)
             {
                 item.UpdateQualityInClass();
             }
-
-            /*
-            for (var i = 0; i < Items.Count; i++)
-            {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (Items[i].Quality > 0)
-                    {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            Items[i].Quality = Items[i].Quality - 1;
-                        }
-                    }
-                }
-                else
-                {
-                    if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].SellIn < 11)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-
-                            if (Items[i].SellIn < 6)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    Items[i].SellIn = Items[i].SellIn - 1;
-                }
-
-                if (Items[i].SellIn < 0)
-                {
-                    if (Items[i].Name != "Aged Brie")
-                    {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].Quality > 0)
-                            {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    Items[i].Quality = Items[i].Quality - 1;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                        }
-                    }
-                    else
-                    {
-                        if (Items[i].Quality < 50)
-                        {
-                            Items[i].Quality = Items[i].Quality + 1;
-                        }
-                    }
-                }
-            }
-        }
-*/
         }
 
         public class Item
